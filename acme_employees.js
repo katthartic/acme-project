826f11e8ecfd9e3649b4516fcfdc6a14609cec78
Katt Baum <mkb2@orlhs164wk-osx.housing.admin.nyu.edu>
2020-01-21 11:27:21 -0500

    Adjust spacing - attempt to fix github formatting issue

--- acme_employees.js
+++ acme_employees.js
@@ -10,7 +10,7 @@ const employees = [
   { id: 8, name: 'shep Jr.', managerId: 4},
   { id: 99, name: 'lucy', managerId: 1}
 ];
-  
+
 const spacer = (text) => {
   if (!text){
     return console.log('');
@@ -18,10 +18,10 @@ const spacer = (text) => {
   const stars = new Array(5).fill('*').join('');
   console.log(`${stars} ${text} ${stars}`);
 }
-  
+
 spacer('findEmployeeByName Moe')
 // given a name and array of employees, return employee
-  
+
 // LOOP
 // function findEmployeeByName(name, list) {
 //   for (let i = 0; i < list.length; i++) {
@@ -47,13 +47,13 @@ console.log(findEmployeeByName('moe', employees));
 //{ id: 1, name: 'moe' }
 
 spacer('')
-  
+
 spacer('findManagerFor Shep')
 //given an employee and a list of employees, return the employee who is the manager
 
 // LOOP
 // function findManagerFor(employee, list) {
-// 	for (let i = 0; i < list.length; i++) {
+//   for (let i = 0; i < list.length; i++) {
 //     const entry = list[i]
 //     if ('managerId' in employee && employee.managerId === entry.id) return entry
 //   }
@@ -61,23 +61,23 @@ spacer('findManagerFor Shep')
 
 // RECURSION
 // function findManagerFor(employee, list) {
-// 	if (!('managerId' in employee)) return 'Employee has no manager'
-// 	let entry = list[0]
-// 	if (employee.managerId === entry.id) return entry
-// 	let altList = list.slice(1)
-// 	return findManagerFor(employee, altList)
+//   if (!('managerId' in employee)) return 'Employee has no manager'
+//   let entry = list[0]
+//   if (employee.managerId === entry.id) return entry
+//   let altList = list.slice(1)
+//   return findManagerFor(employee, altList)
 // }
 
 // FILTER
 function findManagerFor(employee, list) {
-  return list.filter(entry => employee.managerId === entry.id)
+  return list.filter(entry => employee.managerId === entry.id)[0]
 }
 
 console.log(findManagerFor(findEmployeeByName('shep Jr.', employees), employees));
 //{ id: 4, name: 'shep', managerId: 2 }
 
 spacer('')
-  
+
 spacer('findCoworkersFor Larry')
 //given an employee and a list of employees, return the employees who report to the same manager
 
@@ -91,38 +91,37 @@ spacer('findCoworkersFor Larry')
 //     coworkers.push(entry)
 //   }
 //   let altList = list.slice(1)
-// 	return [...coworkers, ...findCoworkersFor(employee, altList)]
+//   return [...coworkers, ...findCoworkersFor(employee, altList)]
 // }
 
 // FILTER
 function findCoworkersFor(employee, list) {
-	return list.filter(entry => employee.managerId === entry.managerId && employee.id !== entry.id)
+  return list.filter(entry => employee.managerId === entry.managerId && employee.id !== entry.id)
 }
-  
+
 console.log(findCoworkersFor(findEmployeeByName('larry', employees), employees));
 /*[ { id: 3, name: 'curly', managerId: 1 },
   { id: 99, name: 'lucy', managerId: 1 } ]
 */
 
 spacer('');
-  
+
 spacer('findManagementChain for moe')
 //given an employee and a list of employees, return a the management chain for that employee. The management chain starts from the employee with no manager with the passed in employees manager 
 
 // RECURSION & FILTER
 function findManagementChainForEmployee(employee, list) {
-	let chain = []
+  let chain = []
   if (!('managerId' in employee)) return chain
-	chain = list.filter(entry => entry.id === employee.managerId)
+  chain = list.filter(entry => entry.id === employee.managerId)
   return [...findManagementChainForEmployee(chain[0], list), ...chain]
 }
-  
   
 console.log(findManagementChainForEmployee(findEmployeeByName('moe', employees), employees));
 //[  ]
 
 spacer('');
-  
+
 spacer('findManagementChain for shep Jr.')
 console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees));
 /*
@@ -132,25 +131,24 @@ console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employ
 */
 
 spacer('');
-  
+
 spacer('generateManagementTree')
 //given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
-
 
 // FILTER, MAP, & RECURSION
 function generateManagementTree(list) {
   let tree = list[0]
-    
-	function internalTree(employee, orgList) {
-		employee.reports = []
-		employee.reports = orgList.filter(entry => employee.id === entry.managerId)
-		employee.reports.map(entry => internalTree(entry, orgList))
+
+  function internalTree(employee, orgList) {
+    employee.reports = []
+    employee.reports = orgList.filter(entry => employee.id === entry.managerId)
+    employee.reports.map(entry => internalTree(entry, orgList))
   }
 
-	internalTree(tree, list)
-	return tree
+  internalTree(tree, list)
+  return tree
 }
-  
+
 console.log(JSON.stringify(generateManagementTree(employees), null, 2));
 /*
 {
@@ -208,24 +206,24 @@ console.log(JSON.stringify(generateManagementTree(employees), null, 2));
 */
 
 spacer('');
-  
+
 spacer('displayManagementTree')
 //given a tree of employees, generate a display which displays the hierarchy
 
 // MAP & RECURSION
 function displayManagementTree(orgTree) {
-	console.log(orgTree.name)
+  console.log(orgTree.name)
 
-	const innerTree = (arr, count) => {
+  const innerTree = (arr, count) => {
     const dashes = new Array(count).fill('-').join('')
-	  arr.map(entry => {
+    arr.map(entry => {
       console.log(`${dashes}${entry.name}`)
       count++
       innerTree(entry.reports, count)
       count--
-	  })
-	}
-	innerTree(orgTree.reports, 1)
+    })
+  }
+  innerTree(orgTree.reports, 1)
 }
 
 displayManagementTree(generateManagementTree(employees));
