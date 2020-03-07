b3395a677bf090376653f42814b3d457baa6b211
katthartic <katt.baum@gmail.com>
2020-03-06 22:37:01 -0500

    Broke something

--- express-practice-2/db.js
+++ express-practice-2/db.js
@@ -1,22 +1,52 @@
-const users = [
-  {
-    id: 1,
-    name: 'moe'
-  },
-  {
-    id: 2,
-    name: 'curly'
-  },
-  {
-    id: 3,
-    name: 'larry'
-  }
-]
+const fs = require('fs')
+const FILE = './users.json'
+
+const readJSON = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(FILE, (err, data) => {
+      if (data) {
+        try {
+          resolve(JSON.parse(data.toString()))
+        } catch (ex) {
+          reject(ex)
+        }
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+const writeJSON = data => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(FILE, JSON.stringify(data, null, 2), err => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+}
 
 const getUsers = () => {
-  return users
+  return readJSON()
+}
+
+const createUser = user => {
+  return getUsers()
+    .then(users => {
+      const maxId = users.reduce((acc, _user) => {
+        if (_user.id > acc) acc = _user.id
+        return acc
+      }, 0)
+      user.id = maxId + 1
+      users.push(user)
+      return writeJSON(users)
+    })
+    .then(() => {
+      return user
+    })
 }
 
 module.exports = {
-  getUsers
+  getUsers,
+  createUser
 }
--- express-practice-2/server.js
+++ express-practice-2/server.js
@@ -1,15 +1,46 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const db = require('./db') //create module
+const db = require('./db')
+
+// ('./users.json', (item, items) => {
+//   if (!item.name) {
+//     return 'name is required'
+//   }
+//   if (items.map(i => i.name).includes(item.name)) {
+//     return 'name must be unique'
+//   }
+// })
+
+app.use((req, res, next) => {
+  console.log(`called ${req.method} on ${req.url}`)
+  next()
+})
+
+app.use(express.json()) //middleware
 
 console.log(path.join(__dirname, 'index.html'))
+
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.get('/api/users', (req, res, next) => {
-  res.send(db.getUsers())
+app.get('/api/users', async (req, res, next) => {
+  try {
+    res.send(await db.getUsers())
+  } catch (ex) {
+    next(ex)
+  }
 })
+
+app.post('/api/users', async (req, res, next) => {
+  try {
+    res.send(await db.createUser(req.body))
+  } catch (ex) {
+    next(ex)
+  }
+})
+
+// db.createUser({ name: 'Bauer' }).then(console.log)
 
 app.listen(3000, () => console.log('listening on port 3000'))
