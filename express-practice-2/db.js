55dfa319e2b123a29cc07d27333d59199641e1eb
katthartic <katt.baum@gmail.com>
2020-03-06 20:33:47 -0500

    intial commit

---
+++ express-practice-2/db.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: 1,
+    name: 'moe'
+  },
+  {
+    id: 2,
+    name: 'curly'
+  },
+  {
+    id: 3,
+    name: 'larry'
+  }
+]
+
+const getUsers = () => {
+  return users
+}
+
+module.exports = {
+  getUsers
+}
